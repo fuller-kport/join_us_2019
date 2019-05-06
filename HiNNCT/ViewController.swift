ccc06a84b2fcf8451ddf6e5743de5a4e0e52c0c7
Sota Hatakeyama <choo.bla.rin@gmail.com>
2019-05-06 13:57:31 +0900

    Initial Commit

---
+++ HiNNCT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiNNCT
+//
+//  Created by Sota Hatakeyama on 2019/05/06.
+//  Copyright © 2019 Sota Hatakeyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
